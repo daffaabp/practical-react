d1329f1275f43ad3de5f4ba6d5dc55f0ffe6d29f
daffaabp <daffa.budi2003@gmail.com>
2024-03-21 01:20:09 +0700

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+const Body = props => (<div>
+  <p className="App-intro">{props.text}</p>
+  <p className="App-intro">{props.text2}</p>
+  <p className="App-intro">{props.myFunc(1,2) }</p>
+</div>
+);
+
+class Header extends Component {
+  render() {
+  const objectA = JSON.stringify(this.props.myObj.a)
+    return (
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">{this.props.title}</h1>
+        <div>{this.props.num}</div>
+        <div>{this.props.myObj.a}</div>
+        <div>{this.props.myFunc(10, 12)}</div>
+        <div>{objectA}</div>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+ add(a, b) {
+  return a + b;
+ }
+  render() {
+    return (
+      <div className="App">
+        <Header 
+        title={"Hello"} 
+        num={7}
+        myArr={[1, 2, 3]}
+        myFunc = {this.add}
+        myObj={{ 
+          a: 5,
+          b: 6
+         }}
+         />
+        <Body myFunc={this.add} text="I'am Cool" text2="I'am also Cool2" />
+        <Body myFunc={this.add} text="I'am Cool" text2="I'am also Cool2" />
+      </div>
+    );
+  }
 }
 
 export default App;
