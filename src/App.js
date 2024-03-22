93ac1de9fb8ea75640d723e9b9b357b642b651f2
daffaabp <daffa.budi2003@gmail.com>
2024-03-22 21:20:05 +0700

    image slider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Body, {Body2, Body3} from "./components/Body";
 import Header from "./components/Header";
 import Counter from "./components/Counter";
+import ImageSlider from "./components/ImageSlider";
 
 
 class App extends Component {
@@ -12,22 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header 
-        title={"Hello"} 
-        num={7}
-        myArr={[1, 2, 3]}
-        myFunc = {this.add}
-        myObj={{ 
-          a: 5,
-          b: 6
-         }}
-         />
-         
-        <Body2 />
-        <Body3 />
-        <Body myFunc={this.add} text="I'am Cool" text2="I'am also Cool2" />
-        <Counter initialCount={0} />
-        <Counter initialCount={10} />
+        <ImageSlider />
       </div>
     );
   }
